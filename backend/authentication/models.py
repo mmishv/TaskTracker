a3735cea528a8287e03f809dc257d36f4657b5fc
maria <pinina7773@gmail.com>
2023-07-26 21:29:44 +0300

    add user permissions check

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
 from django.db.models import Q
 from django.contrib.auth.hashers import make_password
@@ -34,7 +34,7 @@ class UserManager(BaseUserManager):
             return None
 
 
-class User(AbstractBaseUser):
+class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
@@ -49,3 +49,12 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.email
+
+    def has_perm(self, perm, obj=None):
+        if self.is_superuser:
+            return True
+
+        return False
+
+    def has_module_perms(self, app_label):
+        return True
