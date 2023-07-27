5e91a8d649fd286d31bcb3ec0c7b6755bee9ef5e
maria <pinina7773@gmail.com>
2023-07-27 23:27:38 +0300

    update task model

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -12,4 +12,4 @@ class TaskSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'created_at', 'updated_at')
+        fields = '__all__'
