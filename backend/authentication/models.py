0b5ec1fec9c498f2f34291b7c2d0772b97df0ffb
maria <pinina7773@gmail.com>
2023-07-27 21:15:49 +0300

    add get_user for user manager

--- backend/authentication/models.py
+++ backend/authentication/models.py
@@ -33,6 +33,13 @@ class UserManager(BaseUserManager):
         else:
             return None
 
+    def get_user(self, user_id):
+        User = get_user_model()
+        try:
+            return User.objects.get(pk=user_id)
+        except User.DoesNotExist:
+            return None
+
 
 class User(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
